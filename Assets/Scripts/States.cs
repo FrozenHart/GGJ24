d54dd9b8671ca23c2a43ba633ba28cc4005ba4b2
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2024-01-27 13:41:01 +0000

    mainmenu

--- Assets/Scripts/States.cs
+++ Assets/Scripts/States.cs
@@ -57,4 +57,8 @@ public class States : MonoBehaviour
         SceneManager.LoadSceneAsync("Shop");
     }
 
+    public void MainMenu_OnClick()
+    {
+        SceneManager.LoadSceneAsync("MainMenu");
+    }
 }
