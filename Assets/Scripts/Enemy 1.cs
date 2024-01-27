26ad30fd149c715c560ba975b9b87bd5d7f5a1f2
FrozenHart <filipe.neves.esteves.4@gmail.com>
2024-01-27 13:15:34 +0000

    start enemys

--- Assets/Scripts/Enemy 1.cs
+++ Assets/Scripts/Enemy 1.cs
@@ -1,22 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy1 : MonoBehaviour
 {
     [SerializeField]
-    private int boringness = 100; // aka life
+    private float boringness = 0.50f; 
     private List<Effects> effects;
+    public Slider LaghBar;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        LaghBar.value = boringness;
     }
 
     // Update is called once per frame
     void Update()
+    { 
+        LaghBar.value = boringness;
+    }
+
+    public void SetSlider(Slider slider)
     {
-        
+        LaghBar = slider;
     }
 }
