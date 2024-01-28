734003b1c382850c7a2e325fab54b355099ce7c7
David Ferreira da Silva Marques <1221276@isep.ipp.pt>
2024-01-28 14:15:04 +0000

    novas piadas

--- Assets/Scripts/GameManager/DefaultGameStorage.cs
+++ Assets/Scripts/GameManager/DefaultGameStorage.cs
@@ -5,118 +5,121 @@ public static class DefaultGameStorage
 {
     public static List<Enemy> enemyList = new List<Enemy>()
     {
-        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.Dark,HumourType.x),HumourType.Banana),
-        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.Intelligent,HumourType.x),HumourType.Banana),
-        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.Dark,HumourType.Intelligent),HumourType.Banana),
-        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.Dark,HumourType.x),HumourType.Intelligent),
-        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.Dark,HumourType.Banana),HumourType.Intelligent),
-        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.Banana,HumourType.x),HumourType.Intelligent),
-        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.Intelligent,HumourType.x),HumourType.Dark),
-        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.Banana,HumourType.x),HumourType.Dark),
-        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.Banana,HumourType.Intelligent),HumourType.Dark),
-        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.Dark,HumourType.Banana),HumourType.x),
-        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.Dark,HumourType.Intelligent),HumourType.x),
-        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.Banana,HumourType.Intelligent),HumourType.x),
+        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.C,HumourType.D),HumourType.A),
+        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.B,HumourType.D),HumourType.A),
+        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.C,HumourType.B),HumourType.A),
+        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.C,HumourType.D),HumourType.B),
+        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.C,HumourType.A),HumourType.B),
+        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.A,HumourType.D),HumourType.B),
+        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.B,HumourType.D),HumourType.C),
+        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.A,HumourType.D),HumourType.C),
+        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.A,HumourType.B),HumourType.C),
+        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.C,HumourType.A),HumourType.D),
+        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.C,HumourType.B),HumourType.D),
+        new Enemy(0.5f,new Tuple<HumourType, HumourType>(HumourType.A,HumourType.B),HumourType.D),
     };
 
-    public static HashSet<HumourType> AB = new HashSet<HumourType>() {
+    static HashSet<HumourType> AB = new HashSet<HumourType>() {
 
-        HumourType.Banana,
-        HumourType.Intelligent
+        HumourType.A,
+        HumourType.B
     };
 
-    public static HashSet<HumourType> AC = new HashSet<HumourType>() {
+    static HashSet<HumourType> AC = new HashSet<HumourType>() {
 
-        HumourType.Banana,
-        HumourType.Dark
+        HumourType.A,
+        HumourType.C
     };
 
-    public static HashSet<HumourType> AD = new HashSet<HumourType>() {
+    static HashSet<HumourType> AD = new HashSet<HumourType>() {
 
-        HumourType.Banana,
-        HumourType.x
+        HumourType.A,
+        HumourType.D
     };
 
-    public static HashSet<HumourType> BC = new HashSet<HumourType>() {
+    static HashSet<HumourType> BC = new HashSet<HumourType>() {
 
-        HumourType.Dark,
-        HumourType.Intelligent
+        HumourType.C,
+        HumourType.B
     };
 
-    public static HashSet<HumourType> BD = new HashSet<HumourType>() {
+    static HashSet<HumourType> BD = new HashSet<HumourType>() {
 
-        HumourType.x,
-        HumourType.Intelligent
+        HumourType.D,
+        HumourType.B
     };
 
-    public static HashSet<HumourType> CD = new HashSet<HumourType>() {
+    static HashSet<HumourType> CD = new HashSet<HumourType>() {
 
-        HumourType.Dark,
-        HumourType.x
+        HumourType.C,
+        HumourType.D
     };
 
-    public static HashSet<HumourType> ABC = new HashSet<HumourType>() {
+    static HashSet<HumourType> ABC = new HashSet<HumourType>() {
 
-        HumourType.Banana,
-        HumourType.Intelligent,
-        HumourType.Dark
+        HumourType.A,
+        HumourType.B,
+        HumourType.C
     };
 
-    public static HashSet<HumourType> ABD = new HashSet<HumourType>() {
+    static HashSet<HumourType> ABD = new HashSet<HumourType>() {
 
-        HumourType.Banana,
-        HumourType.Intelligent,
-        HumourType.x
+        HumourType.A,
+        HumourType.B,
+        HumourType.D
     };
 
-    public static HashSet<HumourType> ACD = new HashSet<HumourType>() {
+    static HashSet<HumourType> ACD = new HashSet<HumourType>() {
 
-        HumourType.Banana,
-        HumourType.Dark,
-        HumourType.x
+        HumourType.A,
+        HumourType.C,
+        HumourType.D
     };
 
-    public static HashSet<HumourType> BCD = new HashSet<HumourType>() {
+    static HashSet<HumourType> BCD = new HashSet<HumourType>() {
 
-        HumourType.Dark,
-        HumourType.Intelligent,
-        HumourType.x
+        HumourType.C,
+        HumourType.B,
+        HumourType.D
     };
 
-    public static HashSet<HumourType> super = new HashSet<HumourType>() {
-        HumourType.Banana,
-        HumourType.Dark,
-        HumourType.Intelligent,
-        HumourType.x
+    static HashSet<HumourType> super = new HashSet<HumourType>() {
+        HumourType.A,
+        HumourType.C,
+        HumourType.B,
+        HumourType.D
     };
 
-    public static List<Card> cardList = new List<Card>()
-    {
+
+    public static List<Card> GameCards = new List<Card>() {
+
         new Card("Porque é que a galinha atravessou a estrada?", -1, AB, -1, "Para ir para o outro lado da rua!", ShopType.Basic),
         new Card("De que cor é o cavalo branco de napoleão", -1, AB, -1, "...", ShopType.Basic),
-        new Card("O humor desta piada era tão negro...", -1, AC, -1, "...que infelizmente foi baleada pela policia", ShopType.Basic),
-        new Card("Qual a diferença entre um padre e um tenista?", -1, AC, -1, "As bolas com que o tenista brinca têm pelinhos.", ShopType.Basic),
-        new Card("Como é que se chama o protagonista do Dragon Ball com hemorroidas", -1, AD, -1, "San grocu", ShopType.Basic),
-        new Card("truz-truz!", -1, AD, -1, "Quem é? truz-truz!\nQuem é? truz-truz!\nQuem é?...", ShopType.Basic),
-        new Card("Esta piada era suposta ser humor negro e inteligente.", -1, BC, -1,
-            "Infelizmente são duas coisas coisas que não combinam", ShopType.Basic),
+        new Card("Qual é o título de uma notícia em que uma baleia mata outra a tiro?", -1, AC, -1, "Baleia baleia baleia.", ShopType.Basic),
+        new Card("Como é que se chama um cão sem patas?", -1, AC, -1, "Não se chama vai se buscar.", ShopType.Basic),
+        new Card("Como é que se chama o protagonista do Dragon Ball com hemorroidas", -1, AD, -1, "San grocu.", ShopType.Basic),
+        new Card("truz-truz!", -1, AD, -1, "Quem é?\ntruz-truz!\nQuem é?\ntruz-truz!\nQuem é?...", ShopType.Basic),
+        new Card("Porque é que o peiDe congelado é mais caro que o peiDe fresco?", -1, BC, -1,
+            "Porque o peiDe congelado inclui a sobremesa.", ShopType.Basic),
         new Card("O que é que o Kurt Cobain e o Michaelangelo têm em comum?", -1, BC, -1,
             "Ambos usaram o seu cerebro para pintar o teto", ShopType.Basic),
         new Card("Conversa entre dois licenciados", -1, BD, -1, "-Era um BigMac por favor", ShopType.Basic),
-        new Card("carta10", -1, BD, -1, "desc10", ShopType.Basic),
-        new Card("Qual é a parte mais dificil de comer num vegetal?", -1, CD, -1, "A cadeira de rodas", ShopType.Basic),
-        new Card("O que é pior que um bebé dentro de um caixote do lixo?", -1, CD, -1, "Um bebé dentro de dois", ShopType.Basic),
-
-        new Card("Quantos bebés são precisos para mudar a luz da minha cave?", -1, ABC, -1, "Aparentemente mais que vinte porque a minha cave continua às escuras", ShopType.Special),
+        new Card("Havia um cão que respirava pelo cu.", -1, BD, -1, "Sentou-se e morreu", ShopType.Basic),
+        new Card("O que fazem duas cenouras a discutir?", -1, CD, -1, "Poluição cenoura", ShopType.Basic),
+        new Card("-Vamos comer crianças!", -1, CD, -1, "-Vamos comer, crianças!\nUsem virgulas e salvem vidas!", ShopType.Basic),
+        /*new Card("Quantos bebés são precisos para mudar a luz da minha cave?", -1, ABC, -1, 
+            "Aparentemente mais que vinte porque a minha cave continua às escuras", ShopType.Special),
         new Card("O que é que acontece quando misturas adn de tigre com adn humano", -1, ABC, -1,
-            "És expulso do jardim zoológico", ShopType.Special),
-        new Card("carta15", -1, ABD, -1, "desc15", ShopType.Special),
+            "És eDpulso do jardim zoológico", ShopType.Special),*/
+        new Card("", -1, ABD, -1, "desc15", ShopType.Special),
         new Card("carta16", -1, ABD, -1, "desc16", ShopType.Special),
         new Card("carta17", -1, ACD, -1, "desc17", ShopType.Special),
         new Card("carta18", -1, ACD, -1, "desc18", ShopType.Special),
         new Card("carta19", -1, BCD, -1, "desc19", ShopType.Special),
         new Card("carta20", -1, BCD, -1, "desc20", ShopType.Special),
+        new Card("", -1, super, -1, "", ShopType.Super)
 
-        new Card("Cópia Barata do Especial do Dave Chapelle", -1, super, -1, "Acabou-se a inspiração", ShopType.Super)
     };
+
+
 }
