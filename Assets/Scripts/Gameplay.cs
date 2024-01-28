0aefd996d66dd918f5c3cae2a303873db2ac099b
FrozenHart <filipe.neves.esteves.4@gmail.com>
2024-01-28 05:20:04 +0000

    Start Player Hand UI

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using Unity.VisualScripting;
+using System.Linq;
 
 public class Gameplay : MonoBehaviour
 {
@@ -15,38 +16,68 @@ public class Gameplay : MonoBehaviour
     private GameObject Enemy_Mid, Enemy_Left, Enemy_Right;
     public Animator Enemy_Mid_Animator, Enemy_Left_Animator, Enemy_Right_Animator;
     private Enemy enemy_mid, enemy_left, enemy_right;
+    private Enemy curentmid,curentleft, curentright;
     private bool Level1=false, Level2=false, Level3=false;
     public object Midle_Slicder { get; private set; }
-    bool trade = false;
-
+    bool Pass = false;
+    int Level = 1;
     // Start is called before the first frame update
     void Start()
     {
         confirmationDialog.SetActive(false);
-        Level_type_123();
+        NexLevel();
     }
     private void FixedUpdate()
     {
-        if (Level1)
+        if (!GameManager.player.GetHand().Any<Card>())
         {
-            Midle_Slider.GetComponent<Slider>().value = enemy_mid.Get_LaughPower();
-            Enemy_Mid_Animator.SetFloat("laughpower", enemy_mid.Get_LaughPower());
+            if(Level1)
+            {
+                if(enemy_mid.Get_LaughPower() >= 0.75f)
+                {
+                    Pass = true;
+                }
+            }
+            else if (Level2)
+            {
+                if ((enemy_right.Get_LaughPower() >= 0.75f) && (enemy_left.Get_LaughPower() >= 0.75f))
+                {
+                    Pass = true;
+                }
+            }
+            else if (Level3)
+            {
+                if ((enemy_mid.Get_LaughPower() >= 0.75f) && (enemy_right.Get_LaughPower() >= 0.75f) && (enemy_left.Get_LaughPower() >= 0.75f))
+                {
+                    Pass = true;
+                }
+            }
+            if(Pass) { Level++; }
+            NexLevel();
         }
-        else if (Level2)
+        else
         {
-            Left_Slider.GetComponent<Slider>().value = enemy_left.Get_LaughPower();
-            Enemy_Left_Animator.SetFloat("laughpower", enemy_left.Get_LaughPower());
-            Right_Slider.GetComponent<Slider>().value = enemy_right.Get_LaughPower();
-            Enemy_Right_Animator.SetFloat("laughpower", enemy_right.Get_LaughPower());
-        }
-        else if (Level3)
-        {
-            Left_Slider.GetComponent<Slider>().value = enemy_left.Get_LaughPower();
-            Enemy_Left_Animator.SetFloat("laughpower", enemy_left.Get_LaughPower());
-            Right_Slider.GetComponent<Slider>().value = enemy_right.Get_LaughPower();
-            Enemy_Right_Animator.SetFloat("laughpower", enemy_right.Get_LaughPower());
-            Midle_Slider.GetComponent<Slider>().value = enemy_mid.Get_LaughPower();
-            Enemy_Mid_Animator.SetFloat("laughpower", enemy_mid.Get_LaughPower());
+            if (Level1)
+            {
+                Midle_Slider.GetComponent<Slider>().value = enemy_mid.Get_LaughPower();
+                Enemy_Mid_Animator.SetFloat("laughpower", enemy_mid.Get_LaughPower());
+            }
+            else if (Level2)
+            {
+                Left_Slider.GetComponent<Slider>().value = enemy_left.Get_LaughPower();
+                Enemy_Left_Animator.SetFloat("laughpower", enemy_left.Get_LaughPower());
+                Right_Slider.GetComponent<Slider>().value = enemy_right.Get_LaughPower();
+                Enemy_Right_Animator.SetFloat("laughpower", enemy_right.Get_LaughPower());
+            }
+            else if (Level3)
+            {
+                Left_Slider.GetComponent<Slider>().value = enemy_left.Get_LaughPower();
+                Enemy_Left_Animator.SetFloat("laughpower", enemy_left.Get_LaughPower());
+                Right_Slider.GetComponent<Slider>().value = enemy_right.Get_LaughPower();
+                Enemy_Right_Animator.SetFloat("laughpower", enemy_right.Get_LaughPower());
+                Midle_Slider.GetComponent<Slider>().value = enemy_mid.Get_LaughPower();
+                Enemy_Mid_Animator.SetFloat("laughpower", enemy_mid.Get_LaughPower());
+            }
         }
     }
     // Update is called once per frame
@@ -127,4 +158,46 @@ public class Gameplay : MonoBehaviour
         Enemy_Left.SetActive(false);
         Enemy_Right.SetActive(false);
     }
+
+    private void NexLevel()
+    {
+        switch (Level)
+        {
+            case 1:
+                Level_type_123();
+                break;
+            case 2:
+                Level_type_123();
+                break;
+            case 3:
+                Level_type_123();
+                break;
+            case 4:
+                Level_type_456();
+                break;
+            case 5:
+                Level_type_456();
+                break;
+            case 6:
+                Level_type_456();
+                break;
+            case 7:
+                Level_type_7();
+                break;
+        }
+    }
+
+    public void Select_Card_1()
+    {
+
+    }
+    public void Select_Card_2()
+    {
+
+    }
+    public void Select_Card_3()
+    {
+
+    }
+
 }
